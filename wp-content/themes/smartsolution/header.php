d47834a3094a39491ea9cb38077a24a82a5dbbb4
Feel <filimonchuk.alexander@gmail.com>
2016-09-08 14:52:14 +0300

    first commit

---
+++ wp-content/themes/smartsolution/header.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+
+<html lang="ru">
+
+	<title>Smart Solution</title>
+
+	<meta charset="utf-8">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
+
+
+	<script src="js/jquery-3.1.0.min.js" defer></script>
+
+	<script src="js/bootstrap.js" defer></script>
+
+	<script src="js/menumobile.js" defer></script>
+
+
+
+	<header class="header">
+
+		<div class="logo"><a href="/">
+
+				<img class="img_logo" src="img/logo_smartsolution.svg" alt="Логотип компании Smart Solution"></a>
+
+		</div>
+
+		<nav class="header_nav">
+
+			<ul class="nav nav-pills header_nav_menu">
+
+				<li><a href="index_security.html">Категории</a>
+
+				<li><a href="#">Доставка и оплата</a>
+
+				<li><a href="#">О магазине</a>
+
+			</ul>
+
+		</nav>
+
+		<div class="currency">
+
+			<ul class="nav nav-pills menu_currency">
+
+				<li><a href="#">&#36;</a>
+
+				<li><a href="#">&euro;</a>
+
+				<li><a href="#">&#8372;</a>
+
+			</ul>
+
+		</div>
+
+		<div class="user_corner">
+
+			<div class="wr_search_tel">
+
+				<form class="search"><input type="text"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></form>
+
+				<div class="wr_tel_menu">
+
+					<div class="tel">
+
+						<a href="tel:+380444567898">044 456-78-98</a>
+
+						<a href="tel:+380506785687">050 678-56-87</a>
+
+					</div>
+
+					<div class="menu_mobile"><span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span></div>
+
+				</div>
+
+			</div>
+
+			<div class="wr_cart_feedback">
+
+				<div class="cart">
+
+					<i class="fa fa-shopping-cart fa-2x" aria-hidden="true"></i>
+
+					<p><span>0</span> товаров</p>
+
+				</div>
+
+				<div class="feedback">
+
+					<i class="fa fa-phone fa-2x" aria-hidden="true"></i>
+
+					<p>Обратный звонок</p>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</header>
